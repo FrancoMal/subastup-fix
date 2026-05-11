0c08822b12badbf0157f71251477b88eebf97d1c
tsamaan <tsamaanpiroli@uade.edu.ar>
2026-05-11 16:06:00 -0300

    Limpiar rama: eliminar todos los archivos

--- backend/src/routes/bidRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authMiddleware } = require('../middlewares/authMiddleware');
-const bidController = require('../controllers/bidController');
-
-// POST /bids
-router.post('/', authMiddleware, bidController.placeBid);
-
-module.exports = router;
--- backend/src/routes/helpRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const helpController = require('../controllers/helpController');
-
-// GET /help/faq
-router.get('/faq', helpController.getFaq);
-
-module.exports = router;
